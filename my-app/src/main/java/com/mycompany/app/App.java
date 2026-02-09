e8d4ad79fee1c7cbfdb9aa65212ca2350fefbab1
Jose Daniel <jobustamantea@unale.edu.co>
2026-02-08 20:17:25 -0500

    Deleting code related to Threads in App Class

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -2,39 +2,7 @@ package com.mycompany.app;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-s
 public class App {
     public static void main(String[] args) {
-        try{
-            ExecutorService service = Executors.newCachedThreadPool();
-            for(int i = 0; i<10 ;i++){
-                service.execute(new Task(i));
-            }
-            service.shutdown();
-            
-        }
-        catch(Error e){
-            System.out.println("soy un error");
-        }
     }
-}
-
-class Task implements Runnable{
-    private final int taskId;
-
-    public Task(int taskId){
-        this.taskId = taskId;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Task with ID" + taskId + " being executed by Thread " + Thread.currentThread().getName());
-        try{
-            Thread.sleep(100);
-        }catch(InterruptedException e){
-            throw new UnsupportedOperationException("Unimplemented method 'run'");
-
-        }
-    }
-    
 }
